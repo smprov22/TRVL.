bfd0db7316e513e66a8ddbf43a78e691c6d19795
smprov22 <smprovence22@gmail.com>
2019-05-03 15:04:20 -0700

    formatting-fixes

--- assets/js/project1.js
+++ assets/js/project1.js
@@ -1,73 +1,64 @@
-//hide widget on page load
-$("#dump-safety-here").hide();
-$("#dump-outdoor-here").hide();
-$("#weather-here").hide();
-$(".tripRow").hide();
-$("#info").hide();
-$("#dotOnLoad").hide();
+  //hide widget on page load
+  $("#dump-safety-here").hide();
+  $("#dump-outdoor-here").hide();
+  $("#weather-here").hide();
+  $(".tripRow").hide();
+  $("#info").hide();
+  $("#dotOnLoad").hide();
 
 //--------------------------------------WEATHER API ---------------------------------------->
-function weatherApi(city){
-var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
- "q=" + city + ",Burundi&units=imperial&appid=" + APIKey;
+function weatherApi(city) {
+  var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" + city + ",Burundi&units=imperial&appid=" + APIKey;
 
- console.log(queryURL);
+  console.log(queryURL);
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
- url: queryURL,
- method: "GET"
-})
- // We store all of the retrieved data inside of an object called "response"
- .then(function(response) {
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
 
-   // Log the queryURL
-   console.log(queryURL);
+      // Log the queryURL
+      console.log(queryURL);
 
-   // Log the resulting object
-   console.log(response);
+      // Log the resulting object
+      console.log(response);
 
-   // Transfer content to HTML
-
-  //  $(".location").html("<h2>" + response.name + "</h2>").prepend('<i class="fas i fa-globe-americas"></i>');
-  //  $("#CWeather").html(" Current Weather").prepend('<i class="fas i fa-cloud-sun"></i>') 
-  //  $(".wind").text(" Wind Speed: " + Math.floor(response.wind.speed)).prepend('<i class="fas i fa-wind"></i>') ; 
-   $(".tmp-degree").text(" Temperature: " + Math.floor(response.main.temp) + "ºF").prepend('<i  class="fas i fa-temperature-high"></i>'); 
-   $(".temperature-description").text( response.weather[0].description );
-
-   $("#weather-here").show();
- });
+      // Transfer content to HTML
+      $(".tmp-degree").text(" Temperature: " + Math.floor(response.main.temp) + "ºF").prepend('<i  class="fas i fa-temperature-high"></i>');
+      $(".temperature-description").text(response.weather[0].description);
+      $("#weather-here").show();
+    });
 };
 
 
 
 
 //--------------------------------------onClick event---------------------------------------->
- $(document).on("click", "#btn-submit", function(event){
-   event.preventDefault();
-   var city = $("#destination").val().trim().split(" ").join("+");
-  //  var citySafety = $("#destination").val().trim().split(" ").join("-");
-   var cityOutdoors = $("#destination").val().trim().split(" ").join("-");
-   var lower = cityOutdoors.toLowerCase();
-   var citySolo = $("#destination").val().trim();
+$(document).on("click", "#btn-submit", function (event) {
+  event.preventDefault();
+  var city = $("#destination").val().trim().split(" ").join("+");
+  var cityOutdoors = $("#destination").val().trim().split(" ").join("-");
+  var lower = cityOutdoors.toLowerCase();
+  var citySolo = $("#destination").val().trim();
 
-   if (city === ""){
-    alert("Must enter text in the box"); //NEED TO CHANGE TO SOMETHING OTHER THAN AN ALERT!!
+  if (city === "") {
     return false;
   }
 
-   database.collection("cities").add({
-     city: $("#destination").val().trim(),
-     date: new Date()
-   });
+  database.collection("cities").add({
+    city: $("#destination").val().trim(),
+    date: new Date()
+  });
 
-   weatherApi(city);
-   displayUnsplashImages(city);
-   outdoorWidget(lower); 
-  //  addCard(city);
-  //safetyWidget(citySafety); (may add back at a later date)
-  
+  weatherApi(city);
+  displayUnsplashImages(city);
+  outdoorWidget(lower);
+
   $("#destination").val("");
 
   // hide jumbotron on click
@@ -75,49 +66,32 @@ $.ajax({
 
   var cityTitle = citySolo.toUpperCase();
   $('#tripName').html("YOUR TRIP TO " + cityTitle)
-  
   $("#prevSearch").html("Previous Searches");
- 
   $(".tripRow").show();
   $("#info").show();
   $("#dotOnLoad").show()
 });
 
-  //-----------deletes card on click---------------->
-  $(document).on('click', '.delete-button', function(e){
-    e.stopPropagation();
-    $(this).closest("tr").remove();
-    id = e.target.getAttribute("data-id");
-    database.collection("cities").doc(id).delete();
-    });
-
-//--------------------------------------SAFETY WIDGET -------------------------------------->
-
-// function safetyWidget(citySafety){
-// //widget link changes via input
-// $("a").attr("href", "https://teleport.org/cities/" + citySafety)
-
-// //widget url changes via input 
-// $('iframe').attr("id", "widget")
-// $("#widget").attr('src',"https://teleport.org/cities/" + citySafety + "/widget/crime/?currency=USD")
-
-// //display on click 
-// $("#dump-safety-here").show();
-
-// }
+//-----------deletes card on click---------------->
+$(document).on('click', '.delete-button', function (e) {
+  e.stopPropagation();
+  $(this).closest("tr").remove();
+  id = e.target.getAttribute("data-id");
+  database.collection("cities").doc(id).delete();
+});
 
 //--------------------------------------OUTDOORS WIDGET -------------------------------------->
-function outdoorWidget(cityOutdoors){
+function outdoorWidget(cityOutdoors) {
   $("a.teleport-widget-link").attr("href", "https://teleport.org/cities/" + cityOutdoors)
 
-//widget url changes via input 
-$('iframe').attr("id", "widget")
-$("#widget").attr('src', "https://teleport.org/cities/" + cityOutdoors + "/widget/outdoors/?currency=USD&citySwitcher=false")
+  //widget url changes via input 
+  $('iframe').attr("id", "widget")
+  $("#widget").attr('src', "https://teleport.org/cities/" + cityOutdoors + "/widget/outdoors/?currency=USD&citySwitcher=false")
 
-//display on click 
-$("#dump-outdoor-here").show();
-   
+  //display on click 
+  $("#dump-outdoor-here").show();
 }
+
 //---------------------------------------------UNSPLASH API------------------------------------------------>
 function displayUnsplashImages(city) {
   var queryURL = "https://api.unsplash.com/search/photos?page=1&query=" + city + "&client_id=98fa38e783accee54b2682447c53324d56d7375e2b0e7708a53172528b223ab7";
@@ -132,15 +106,15 @@ function displayUnsplashImages(city) {
       var imgDiv = $("<div>");
       imgDiv.addClass("carousel-item");
       var showImage = $("<img>");
-      showImage.attr("src", results[i].urls.regular +  "&w=1100&h=600");
+      showImage.attr("src", results[i].urls.regular + "&w=1100&h=600");
       showImage.addClass("d-block");
       imgDiv.prepend(showImage);
- 
+
       $(".carousel-inner").append(imgDiv);
     };
     $('.carousel-item').first().addClass('active');
   })
- }
+}
 
 //----------------------------------------FIREBASE----------------------------------->
 
@@ -157,49 +131,26 @@ var database = firebase.firestore();
 
 function displayCities(doc) {
   var newRow = $("<tr>").addClass("compare-city ").attr("data-id", doc.id).append(
-    $("<button>").text( doc.data().city).addClass("btn btn-light  btn-sm m-0"),
+    $("<button>").text(doc.data().city).addClass("btn btn-light  btn-sm m-0"),
     $("<button>").text("x").addClass("delete-button btn btn-light  btn-sm p-1 m-0").attr("data-id", doc.id),
-    );
-    $("#info").prepend(newRow)
-    // console.log(doc.data().city);
-  }
- 
+  );
+  $("#info").prepend(newRow)
+}
 
-  //-----------------KELLI'S CODE IN PROGRESS----------->
-  // var tableRow= [];
-  // console.log(tableRow);
-  // var newRow = $("<tr>").append(
-  //   $("<td>").text((doc.data().city)),
-  //   $("<button>").text("x").addClass("delete-button").attr("data-id", doc.id),
-  //   );
-    
-// //-----------PRINT ONLY 5 TRIPS------------->
-// for(let i=0; i <5; i++){
-//     if(tableRow.length < 5){
-//       $("#info").prepend(newRow)
-//       tableRow.push(newRow)
-//       console.log("works")
-//     } else {
-//       alert("over 5")
-//       tableRow[0].empty();
-//     }
-
-//   }
-// }
-  database.collection("cities").orderBy("date", "desc").onSnapshot(snapshot => {
-    var changes = snapshot.docChanges();
-    changes.forEach(change => {
-      if (change.type == "added") {
-        displayCities(change.doc);
-      }
-    })
+database.collection("cities").orderBy("date", "desc").onSnapshot(snapshot => {
+  var changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if (change.type == "added") {
+      displayCities(change.doc);
+    }
   })
+})
 
 //---------------------PREVIOUS SEARCH CLICK-------------------//
-$(document).on('click', '.compare-city', function(doc){
+$(document).on('click', '.compare-city', function (doc) {
   var cityClick = $(this).attr("data-id");
   console.log(cityClick);
-  database.collection("cities").doc(cityClick).get().then(function(doc){
+  database.collection("cities").doc(cityClick).get().then(function (doc) {
     if (doc.exists) {
       var reSearch = doc.data().city;
       var outdoors = reSearch.split(" ").join("-");
@@ -211,8 +162,7 @@ $(document).on('click', '.compare-city', function(doc){
       outdoorWidget(outdoorsLower);
 
       var cityTitle = reSearch.toUpperCase();
-  $('#tripName').html("YOUR TRIP TO " + cityTitle)
+      $('#tripName').html("YOUR TRIP TO " + cityTitle)
     }
-   })
-  
   })
+})
